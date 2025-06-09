30ebeb12c958258ed1c87fea94aeb585908f085c
drguandinango <drguandinango@pucesi.edu.ec>
2025-06-09 15:49:03 -0500

    first ingreso al proyecto de node js

---
+++ index.js
@@ -0,0 +1,7 @@
+const os=require('node:os');
+console.log('información del sistema  operativo: ');
+
+console.log('---------------------');
+
+console.log('nombre del sistema operativo', os.platform());
+console.log('nombre del sistema operativo');
