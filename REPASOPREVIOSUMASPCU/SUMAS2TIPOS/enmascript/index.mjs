72afd473c3fe6c5f94213f0bfc2d893122cb7393
unknown <Administrador@SALA2-20.pucesi.edu.ec>
2025-06-10 12:05:33 -0500

    usando el modo de enmascript modules

---
+++ REPASOPREVIOSUMASPCU/SUMAS2TIPOS/enmascript/index.mjs
@@ -0,0 +1,10 @@
+import{sumacion, multiplication} from './sumas.mjs'
+//una por una
+console.log(
+    sumacion(2,12),
+    multiplication(9,4)
+)
+
+//en grupo impresión cómo era , o cómo sería=>?
+
+
---
+++ REPASOPREVIOSUMASPCU/SUMAS2TIPOS/enmascript/sumas.mjs
@@ -0,0 +1,11 @@
+export function sumacion(a,b){
+
+ return  a+b;
+
+}
+
+export function multiplication(a,b){
+
+ return  a*b;
+
+}
