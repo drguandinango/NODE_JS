0b0671d1c046b8239ae0bf2fe55d2a604d5b0a12
drguandinango <drguandinango@pucesi.edu.ec>
2025-06-13 16:40:47 -0500

    hasta el process_obtener el porceos desee donde y el valor de ENV pasaso en varibale de entorno

---
+++ v4REPASO_DESDE-INICIO_TODO_v4/process.js
@@ -0,0 +1,23 @@
+// current working directory
+
+//desdde que parpeta estamsno ejecutanto el proceso
+console.log(process.cwd())
+
+//hay evecs qiue ewjecuto ese archoiv desde otro lugar , 
+//loq eu me dice es desde dond estoy ejecuatndo el proceso
+//desde que carpeta hemos inicializado
+
+
+
+//aquí tambien hay el de variables de entorno
+//------------->
+//la varibles de entorno
+
+//el entorno de si se estpá ejecutando para 
+//producción , o desarrollo
+
+console.log(process.env.PEPITO)
+
+//$env:PEPITO="hola"; node process.js
+
+//el hola es valor de pepito porque se le está pasando como variable de entorno...
