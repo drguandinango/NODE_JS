5c57d9c6802dad83dab24374edffc8ab05c04c94
drguandinango <drguandinango@pucesi.edu.ec>
2025-11-05 12:55:58 -0500

    hasta ls advenced v1 v2 pemndidte terminar unido todo__pero avamzamdo con npm [200~https://youtu.be/yB4n_K7dZV8?t=6563~

---
+++ npmIntroduccion/comandos.js
@@ -0,0 +1,21 @@
+/*
+npm  init
+//escrioidon en json dl proyecto scropts version licencia ets}c
+
+entripoint ese archivo ue al correrlo por node levanta la api x ejemplo 
+
+se CREA 
+
+PACKAGE JSON
+
+es para iniciar los test todo  
+
+pero sin dependencias 
+
+picocolors
+(ayuda a doratr de vdia al console log )
+
+picolors
+
+
+*/
