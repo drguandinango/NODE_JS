a0124dc1694fc79b574751ab080ac262819896cf
drguandinango <drguandinango@pucesi.edu.ec>
2025-11-05 22:36:02 -0500

    5 nov 	https://youtu.be/yB4n_K7dZV8?t=7653

--- npmIntroduccion/comandos.js
+++ npmIntroduccion/comandos.js
@@ -2,20 +2,62 @@
 npm  init
 //escrioidon en json dl proyecto scropts version licencia ets}c
 
-entripoint ese archivo ue al correrlo por node levanta la api x ejemplo 
+entripoint ese archivo ue al correrlo por node levanta la api x ejemplo
 
-se CREA 
+se CRE
 
 PACKAGE JSON
 
-es para iniciar los test todo  
+es para iniciar los test todo
 
-pero sin dependencias 
+pero sin dependencias
 
 picocolors
 (ayuda a doratr de vdia al console log )
 
-picolors
+npm install picolors
 
+version lens para que me dugiera la actuilzizacion de pocolocors
 
+ x  y x
+ major  x
+
+ minor y
+
+ patch se arreglan  bu
+
+ npm unistall react
+
+ npm unistall lodash
+
+ dependecias de produccion o de desarollo
+
+ npm install standard -D{delvelopmemt production-->separacion devdependecess vs dependences}
+
+devDeoendences::::
+   "eslintConfig":{
+    "extends":"standard"
+  }
+
+  ::::::::::::::::::inetereante la paete de json setting....
+
+  ::::::::::::::::::::::
+
+  {
+    // 1. Establecer ESLint como el formateador predeterminado para archivos JS/TS
+    "editor.codeActionsOnSave": {
+        // Habilita la corrección automática de problemas de ESLint al guardar
+        "source.fixAll.eslint": "explicit"
+    },
+
+    // 2. Establecer el formateador predeterminado de VS Code
+    "editor.defaultFormatter": "esbenp.prettier-vscode", // (Si usas Prettier)
+
+    // Si quieres que VS Code formatee el archivo, PERO que ESLint corrija los errores.
+    // Si tienes conflictos entre Prettier y ESLint, necesitarás la integración `eslint-config-prettier`.
+    "editor.formatOnSave": true
+}
+::::::::::::::::::::::::
+
+//semantic versioning
 */
---
+++ npmIntroduccion/http.js
@@ -0,0 +1,67 @@
+/* const http = require('node:http')
+
+const servidor = http.createServer((req, resp) => {
+  console.log(' petición recibida ....')
+  resp.end('Hola mundo')
+})
+
+servidor.listen(3001, () => {
+  console.log('escuchando en el puerto 30001^^')
+})
+  */
+
+/*
+const http = require('node:http')
+
+const servidor = http.createServer((req, resp) => {
+  console.log(' petición recibida ....')
+  resp.end('Hola mundo')
+})
+
+servidor.listen(0, () => {
+  console.log('escuchando en el puerto ????')
+})
+*/
+
+// recuperar el puerto randómico
+/*
+const http = require('node:http')
+
+const servidor = http.createServer((req, resp) => {
+  console.log(' petición recibida ....')
+  resp.end('Hola mundo')
+})
+
+servidor.listen(0, () => {
+  console.log(`escuchando en el puerto ${servidor.address().port}`)
+
+  //
+const http = require('node:http')
+const servidor = http.createServer((req, resp) => {
+  console.log('recibido la pericion')
+  resp.end('hola mundo')
+})
+
+servidor.listen(0, () => {
+  console.log(`escuchando en el puerto http://localhost:${servidor.address().port}`)
+})
+  //
+// // })
+// // })
+// // })
+
+// // })
+// // })
+  // // })
+// // })
+
+*/
+const http = require('node:http')
+const servidor = http.createServer((req, resp) => {
+  console.log('peticion recibida')
+  resp.end('holi mindo')
+})
+
+servidor.listen(0, () => {
+  console.log(`esuchando es el puerto aleatorio http://localhost:${servidor.address().port}`)
+})
